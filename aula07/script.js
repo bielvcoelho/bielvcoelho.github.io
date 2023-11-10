eb721dbe62d91f90ceeb27978ea564ddc6b8c9b7
bielvcoelho <142635868+bielvcoelho@users.noreply.github.com>
2023-11-10 19:53:56 -0300

    Add files via upload

--- aula07/script.js
+++ aula07/script.js
@@ -1,18 +1,67 @@
 console.log(receitaList)
 
+const criarElem = (Tipo, Classe, Prop, Conteudo, elemPai) => {
+
+    const obj = document.createElement(Tipo)
+    obj.classList.add(Classe)
+    obj[Prop] = Conteudo
+
+    elemPai.appendChild(obj)
+
+    return obj
+}
+
 receitaList.forEach(receita =>{
 
     const receitaDiv = document.createElement("div")
     receitaDiv.classList.add("receita")
 
-    const tituloReceitaH3 = document.createElement("h3")
-    tituloReceitaH3.classList.add("receita--titulo")
-    tituloReceitaH3.textContent = receita.nome
-
-    receitaDiv.appendChild(tituloReceitaH3)
-    const receitasContainer = document.querySelector(".receitas")
-    receitasContainer.appendChild(receitaDiv)
+    //titulos
+    criarElem("h3", "receita_titulo", "textContent", receita.nome, receitaDiv)
     
+    
+   //subtitulos
+    criarElem("p", "receita__titulo", "textContent", receita.descricao, receitaDiv)
+
+    //imagens
+    criarElem("img", "receita__figura", "src", receita.foto, receitaDiv)
+
+
+
+
+    //h4 - titulo dos ingredientes
+    criarElem("h4", "receita__ingredientes", "textContent", "Ingredientes", receitaDiv)
+
+    //ul - ingredientes
+    const ulDaVez = criarElem("UL", "receita__ingredientes", "", "", receitaDiv)
+    receita.ingredientes.forEach(item =>{
+        //li
+        criarElem("li", "receita__item", "textContent", item, ulDaVez)
+    })
+
+
+
+
+    //h4 - titulo do preparo
+    criarElem("h4", "receita__preparo-titulo", "textContent", "Modo de Preparo", receitaDiv)
+
+    //ol - modo de preparo
+    const olDaVez = criarElem("ol", "receita__preparo-lista", "", "", receitaDiv)
+    receita.preparo.forEach(item =>{
+        //li
+        criarElem("li", "receita__item", "textContent", item, olDaVez)
+    })
+
+
+
+
+
+
+
+
+
+    const receitaContainer = document.querySelector(".receitas")
+    receitaContainer.appendChild(receitaDiv)
 
 
 })
